b44e84e713dee5917d9efa0f3f91493374d71eed
kobayashikento <kentokobayashik@gmail.com>
2021-02-25 02:55:32 -0500

    capitalization error

--- components/apod-body.js
+++ components/apod-body.js
@@ -4,7 +4,7 @@ export default function ApodBody({ content }) {
     return (
         <Container maxWidth="md" style={{ paddingTop: "4.4vmax", display: "flex", flexDirection: "column" }}>
             <Typography variant="h4" style={{ fontWeight: "bold" }}>
-                Description of the picture:
+                Description of the Picture:
              </Typography>
             <Typography align="justify" variant="body1" style={{ paddingTop: "1.1vmax", textIndent: "1rem" }}
                 dangerouslySetInnerHTML={{ __html: content }}
